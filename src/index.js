9df9b45015ec2fb983b9784d65e04e52dbd80a1c
Shadess <tylermboettcher@gmail.com>
2021-07-16 09:18:58 -0400

    firebase config and analytics

--- src/index.js
+++ src/index.js
@@ -7,6 +7,29 @@ import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBQANQMYdGVPmwqseqoqH41MckboGyITqg",
+  authDomain: "rpg-initiation.firebaseapp.com",
+  databaseURL: "https://rpg-initiation-default-rtdb.firebaseio.com",
+  projectId: "rpg-initiation",
+  storageBucket: "rpg-initiation.appspot.com",
+  messagingSenderId: "418474513340",
+  appId: "1:418474513340:web:a6fdb0841e48a2ff7dd1b9",
+  measurementId: "G-8YPE2VQB2E"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
