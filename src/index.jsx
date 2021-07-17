ab9a02107a05e53d8f5b1d4ecfd56a34d1923bc3
Shadess <tylermboettcher@gmail.com>
2021-07-17 14:37:50 -0400

    moving firebase configs

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { firebaseConfig, uiConfig } from './firebaseConfig';
 
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import firebase from "firebase/app";
@@ -19,46 +19,10 @@ import "firebase/analytics";
 // Add the Firebase products that you want to use
 import "firebase/auth";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBQANQMYdGVPmwqseqoqH41MckboGyITqg",
-  authDomain: "rpg-initiation.firebaseapp.com",
-  databaseURL: "https://rpg-initiation-default-rtdb.firebaseio.com",
-  projectId: "rpg-initiation",
-  storageBucket: "rpg-initiation.appspot.com",
-  messagingSenderId: "418474513340",
-  appId: "1:418474513340:web:a6fdb0841e48a2ff7dd1b9",
-  measurementId: "G-8YPE2VQB2E"
-};
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 const ui = new firebaseui.auth.AuthUI(firebase.auth());
-const uiConfig = {
-  callbacks: {
-    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
-      return true;
-    },
-    uiShown: function() {
-      // The widget is rendered.
-      // Hide the loader.
-      document.getElementById('loader').style.display = 'none';
-    }
-  },
-  // signInSuccessUrl: '<url-to-redirect-to-on-success>',
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-  ],
-  // Terms of service url.
-  // tosUrl: '<your-tos-url>',
-  // Privacy policy url.
-  // privacyPolicyUrl: '<your-privacy-policy-url>'
-};
 ui.start('#firebaseui-auth-container', uiConfig);
 
 ReactDOM.render(
@@ -69,8 +33,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
