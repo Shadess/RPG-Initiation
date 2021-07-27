c1895ec897f9bffb9483188661a4169965d96493
Shadess <tylermboettcher@gmail.com>
2021-07-26 20:22:50 -0400

    updating packages and adding configs

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import './index.css';
 import App from './App';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
 import Firebase, { FirebaseContext } from './components/Firebase';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+Sentry.init({
+  dsn: 'https://798d787dfb154d61ad7300eaa2a734a9@o931386.ingest.sentry.io/5880363',
+  integrations: [new Integrations.BrowserTracing()],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
