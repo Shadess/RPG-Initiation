e42f44b6e33f104ab0e73760c05e43ee1380ef40
Shadess <tylermboettcher@gmail.com>
2021-08-21 08:53:54 -0400

    yarn lint initial fix

--- src/tests/Initiation.test.js
+++ src/tests/Initiation.test.js
@@ -1,10 +1,11 @@
+import { expect } from 'jest';
 import { store } from '../store/store';
 import {
-    addPlayer,
-    clearPlayers,
-    removePlayer,
-    selectPlayers,
-    sort
+  addPlayer,
+  clearPlayers,
+  removePlayer,
+  selectPlayers,
+  sort,
 } from '../store/initiationSlice';
 import Player from '../models/Player';
 
@@ -14,79 +15,106 @@ const testPlayer2 = new Player('john', 18.3);
 const testPlayer3 = new Player('jeff', 12.3);
 
 beforeEach(() => {
-    rootState = store.getState();
+  rootState = store.getState();
 });
 
 afterEach(() => {
-    store.dispatch(clearPlayers());
-})
+  store.dispatch(clearPlayers());
+});
 
 test('initializes store', () => {
-    expect(rootState).toBeDefined();
-    expect(rootState.initiation).toBeDefined();
+  expect(rootState).toBeDefined();
+  expect(rootState.initiation).toBeDefined();
 });
 
 test('gets list of players initialized to empty', () => {
-    expect(selectPlayers(rootState)).toStrictEqual([]);
+  expect(selectPlayers(rootState)).toStrictEqual([]);
 });
 
 test('adds a player', () => {
-    store.dispatch(addPlayer(testPlayer));
-    expect(selectPlayers(store.getState())).toStrictEqual([testPlayer]);
+  store.dispatch(addPlayer(testPlayer));
+  expect(selectPlayers(store.getState())).toStrictEqual([testPlayer]);
 });
 
 test('does not add a non player object', () => {
-    store.dispatch(addPlayer('hey there'));
-    expect(selectPlayers(store.getState())).toStrictEqual([]);
+  store.dispatch(addPlayer('hey there'));
+  expect(selectPlayers(store.getState())).toStrictEqual([]);
 });
 
 test('removes a player from empty', () => {
-    store.dispatch(removePlayer(testPlayer));
-    expect(selectPlayers(store.getState())).toStrictEqual([]);
+  store.dispatch(removePlayer(testPlayer));
+  expect(selectPlayers(store.getState())).toStrictEqual([]);
 });
 
 test('removes an actual player', () => {
-    store.dispatch(addPlayer(testPlayer));
-    store.dispatch(addPlayer(testPlayer2));
-    expect(selectPlayers(store.getState())).toStrictEqual([testPlayer, testPlayer2]);
-    store.dispatch(removePlayer(testPlayer));
-    expect(selectPlayers(store.getState())).toStrictEqual([testPlayer2]);
+  store.dispatch(addPlayer(testPlayer));
+  store.dispatch(addPlayer(testPlayer2));
+  expect(selectPlayers(store.getState())).toStrictEqual([
+    testPlayer,
+    testPlayer2,
+  ]);
+  store.dispatch(removePlayer(testPlayer));
+  expect(selectPlayers(store.getState())).toStrictEqual([testPlayer2]);
 });
 
 test('does not remove a player not in the array', () => {
-    store.dispatch(addPlayer(testPlayer));
-    store.dispatch(addPlayer(testPlayer2));
-    expect(selectPlayers(store.getState())).toStrictEqual([testPlayer, testPlayer2]);
-    store.dispatch(removePlayer(testPlayer3));
-    expect(selectPlayers(store.getState())).toStrictEqual([testPlayer, testPlayer2]);
+  store.dispatch(addPlayer(testPlayer));
+  store.dispatch(addPlayer(testPlayer2));
+  expect(selectPlayers(store.getState())).toStrictEqual([
+    testPlayer,
+    testPlayer2,
+  ]);
+  store.dispatch(removePlayer(testPlayer3));
+  expect(selectPlayers(store.getState())).toStrictEqual([
+    testPlayer,
+    testPlayer2,
+  ]);
 });
 
 test('doest not remove a non player object', () => {
-    store.dispatch(addPlayer(testPlayer));
-    store.dispatch(addPlayer(testPlayer2));
-    expect(selectPlayers(store.getState())).toStrictEqual([testPlayer, testPlayer2]);
-    store.dispatch(removePlayer('testPlayer2'));
-    expect(selectPlayers(store.getState())).toStrictEqual([testPlayer, testPlayer2]);
+  store.dispatch(addPlayer(testPlayer));
+  store.dispatch(addPlayer(testPlayer2));
+  expect(selectPlayers(store.getState())).toStrictEqual([
+    testPlayer,
+    testPlayer2,
+  ]);
+  store.dispatch(removePlayer('testPlayer2'));
+  expect(selectPlayers(store.getState())).toStrictEqual([
+    testPlayer,
+    testPlayer2,
+  ]);
 });
 
 test('does not add same player', () => {
-    store.dispatch(addPlayer(testPlayer));
-    store.dispatch(addPlayer(testPlayer));
-    expect(selectPlayers(store.getState())).toStrictEqual([testPlayer]);
+  store.dispatch(addPlayer(testPlayer));
+  store.dispatch(addPlayer(testPlayer));
+  expect(selectPlayers(store.getState())).toStrictEqual([testPlayer]);
 });
 
 test('sorts array unsorted', () => {
-    store.dispatch(addPlayer(testPlayer));
-    store.dispatch(addPlayer(testPlayer2));
-    expect(selectPlayers(store.getState())).toStrictEqual([testPlayer, testPlayer2]);
-    store.dispatch(sort());
-    expect(selectPlayers(store.getState())).toStrictEqual([testPlayer2, testPlayer]);
+  store.dispatch(addPlayer(testPlayer));
+  store.dispatch(addPlayer(testPlayer2));
+  expect(selectPlayers(store.getState())).toStrictEqual([
+    testPlayer,
+    testPlayer2,
+  ]);
+  store.dispatch(sort());
+  expect(selectPlayers(store.getState())).toStrictEqual([
+    testPlayer2,
+    testPlayer,
+  ]);
 });
 
 test('sorts array sorted', () => {
-    store.dispatch(addPlayer(testPlayer2));
-    store.dispatch(addPlayer(testPlayer3));
-    expect(selectPlayers(store.getState())).toStrictEqual([testPlayer2, testPlayer3]);
-    store.dispatch(sort());
-    expect(selectPlayers(store.getState())).toStrictEqual([testPlayer2, testPlayer3]);
+  store.dispatch(addPlayer(testPlayer2));
+  store.dispatch(addPlayer(testPlayer3));
+  expect(selectPlayers(store.getState())).toStrictEqual([
+    testPlayer2,
+    testPlayer3,
+  ]);
+  store.dispatch(sort());
+  expect(selectPlayers(store.getState())).toStrictEqual([
+    testPlayer2,
+    testPlayer3,
+  ]);
 });
--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,53 +1,54 @@
+import { expect } from 'jest';
 import Player from '../models/Player';
 
 test('initializes class', () => {
-    const player = new Player();
-    expect(player).toBeDefined();
+  const player = new Player();
+  expect(player).toBeDefined();
 });
 
 test('initializes class with values', () => {
-    const player = new Player('john', 3.2);
-    expect(player.GetInitiation()).toBe(3.2);
-    expect(player.GetName()).toBe('john');
+  const player = new Player('john', 3.2);
+  expect(player.GetInitiation()).toBe(3.2);
+  expect(player.GetName()).toBe('john');
 });
 
 test('initializes class with string init', () => {
-    const player = new Player('john2', '5');
-    expect(player.GetInitiation()).toBe(5);
-    expect(player.GetName()).toBe('john2');
+  const player = new Player('john2', '5');
+  expect(player.GetInitiation()).toBe(5);
+  expect(player.GetName()).toBe('john2');
 });
 
 test('initializes class with numeric name', () => {
-    const player = new Player(2, 2.0);
-    expect(player.GetInitiation()).toBe(2.0);
-    expect(player.GetName()).toBe('2');
+  const player = new Player(2, 2.0);
+  expect(player.GetInitiation()).toBe(2.0);
+  expect(player.GetName()).toBe('2');
 });
 
 test('initializes class with bad init', () => {
-    const player = new Player('john3', 'abc');
-    expect(player.GetInitiation()).toBe(0);
-    expect(player.GetName()).toBe('john3');
+  const player = new Player('john3', 'abc');
+  expect(player.GetInitiation()).toBe(0);
+  expect(player.GetName()).toBe('john3');
 });
 
 test('does not equal non Player Object', () => {
-    const player = new Player('james', 2);
-    expect(player.Equals('something')).toBe(false);
+  const player = new Player('james', 2);
+  expect(player.Equals('something')).toBe(false);
 });
 
 test('does equal same Player', () => {
-    const player = new Player('james', 2);
-    const player2 = new Player('james', 2);
-    expect(player.Equals(player2)).toBe(true);
+  const player = new Player('james', 2);
+  const player2 = new Player('james', 2);
+  expect(player.Equals(player2)).toBe(true);
 });
 
 test('does equal same Player with different initiation', () => {
-    const player = new Player('james', 2);
-    const player2 = new Player('james', 12);
-    expect(player.Equals(player2)).toBe(true);
+  const player = new Player('james', 2);
+  const player2 = new Player('james', 12);
+  expect(player.Equals(player2)).toBe(true);
 });
 
 test('does not equal different Player name', () => {
-    const player = new Player('james', 2);
-    const player2 = new Player('walker', 2);
-    expect(player.Equals(player2)).toBe(false);
+  const player = new Player('james', 2);
+  const player2 = new Player('walker', 2);
+  expect(player.Equals(player2)).toBe(false);
 });
